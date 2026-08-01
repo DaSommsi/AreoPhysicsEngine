a1d5253cea6333cf070d8eb657d2f85f3f22f5c8
DaSommsi <david.sommerer.200811@gmail.com>
2026-08-01 21:19:51 +0200

    feat(physics): upgrade inertia tensor to a 3x3 matrix

---
+++ include/physics/Matrix3x3.hpp
@@ -0,0 +1,66 @@
+#pragma once
+
+#include "Vector3.hpp"
+#include "Quaternion.hpp"
+#include <array>
+
+namespace Math {
+
+    struct Matrix3x3 {
+        std::array<double, 9> m{
+            1.0, 0.0, 0.0,
+            0.0, 1.0, 0.0,
+            0.0, 0.0, 1.0
+        };
+
+        Matrix3x3() = default;
+
+        constexpr Matrix3x3(double m00, double m01, double m02,
+                            double m10, double m11, double m12,
+                            double m20, double m21, double m22)
+            : m{m00, m01, m02, m10, m11, m12, m20, m21, m22} {}
+
+        [[nodiscard]] constexpr Vector3 operator*(const Vector3& v) const {
+            return Vector3{
+                m[0] * v.x + m[1] * v.y + m[2] * v.z,
+                m[3] * v.x + m[4] * v.y + m[5] * v.z,
+                m[6] * v.x + m[7] * v.y + m[8] * v.z
+            };
+        }
+
+        [[nodiscard]] constexpr Matrix3x3 operator*(const Matrix3x3& o) const {
+            return Matrix3x3{
+                m[0]*o.m[0] + m[1]*o.m[3] + m[2]*o.m[6],
+                m[0]*o.m[1] + m[1]*o.m[4] + m[2]*o.m[7],
+                m[0]*o.m[2] + m[1]*o.m[5] + m[2]*o.m[8],
+
+                m[3]*o.m[0] + m[4]*o.m[3] + m[5]*o.m[6],
+                m[3]*o.m[1] + m[4]*o.m[4] + m[5]*o.m[7],
+                m[3]*o.m[2] + m[4]*o.m[5] + m[5]*o.m[8],
+
+                m[6]*o.m[0] + m[7]*o.m[3] + m[8]*o.m[6],
+                m[6]*o.m[1] + m[7]*o.m[4] + m[8]*o.m[7],
+                m[6]*o.m[2] + m[7]*o.m[5] + m[8]*o.m[8]
+            };
+        }
+
+        [[nodiscard]] constexpr Matrix3x3 transposed() const {
+            return Matrix3x3{
+                m[0], m[3], m[6],
+                m[1], m[4], m[7],
+                m[2], m[5], m[8]
+            };
+        }
+
+        [[nodiscard]] static constexpr Matrix3x3 from_quaternion(const Quaternion& q) {
+            double x = q.x, y = q.y, z = q.z, w = q.w;
+
+            return Matrix3x3{
+                1.0 - 2.0 * (y*y + z*z), 2.0 * (x*y - z*w),       2.0 * (x*z + y*w),
+                2.0 * (x*y + z*w),       1.0 - 2.0 * (x*x + z*z), 2.0 * (y*z - x*w),
+                2.0 * (x*z - y*w),       2.0 * (y*z + x*w),       1.0 - 2.0 * (x*x + y*y)
+            };
+        }
+    };
+
+}
--- include/physics/RigidBody3D.hpp
+++ include/physics/RigidBody3D.hpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include "Vector3.hpp"
 #include "Quaternion.hpp"
+#include "Matrix3x3.hpp"
 
 namespace Physics {
     
@@ -13,18 +14,46 @@ namespace Physics {
         Math::Vector3 angular_velocity{0.0, 0.0, 0.0};
 
         double mass{1.0};
-        double inertia_tensor{1.0};
+        double inv_mass{1.0};
+        
+        Math::Matrix3x3 inv_inertia_tensor_body;
+        Math::Matrix3x3 inv_inertia_tensor_world;
 
         Math::Vector3 force_accumulator{0.0, 0.0, 0.0};
         Math::Vector3 torque_accumulator{0.0, 0.0, 0.0};
 
         RigidBody3D() = default;
-        RigidBody3D(Math::Vector3 position, Math::Quaternion orientation, double mass, double inertia_tensor) : position(position), orientation(orientation), mass(mass), inertia_tensor(inertia_tensor) {}
-        ~RigidBody3D() = default;
+
+        RigidBody3D(Math::Vector3 position, Math::Quaternion orientation, double mass) 
+            : position(position), orientation(orientation) {
+            set_mass(mass);
+            set_box_inertia(1.0, 1.0, 1.0);
+        }
+
+        void set_mass(double new_mass) {
+            mass = new_mass;
+            inv_mass = (mass > 0.0) ? (1.0 / mass) : 0.0;
+        }
+
+        void set_box_inertia(double width, double height, double depth) {
+            if (inv_mass == 0.0) {
+                inv_inertia_tensor_body = Math::Matrix3x3{0,0,0, 0,0,0, 0,0,0};
+                return;
+            }
+
+            double ix = (1.0 / 12.0) * mass * (height * height + depth * depth);
+            double iy = (1.0 / 12.0) * mass * (width * width + depth * depth);
+            double iz = (1.0 / 12.0) * mass * (width * width + height * height);
+
+            inv_inertia_tensor_body = Math::Matrix3x3{
+                1.0 / ix, 0.0,      0.0,
+                0.0,      1.0 / iy, 0.0,
+                0.0,      0.0,      1.0 / iz
+            };
+        }
 
         void add_force(const Math::Vector3& force){
             force_accumulator += force;
-            std::cout << force_accumulator.x << " | " << force_accumulator.y << " | " << force_accumulator.z << "\n";
         }
 
         void add_force_at_point(const Math::Vector3& force, const Math::Vector3& point){
@@ -37,14 +66,24 @@ namespace Physics {
             torque_accumulator = {0.0, 0.0, 0.0};
         }
 
-        void integrate(double dt){
-            auto a = force_accumulator / mass;
-            linear_velocity += a*dt;
-            position += linear_velocity*dt;
+        void integrate(double dt) {
+            if (inv_mass == 0.0) return;
 
-            auto alpha = torque_accumulator / inertia_tensor;
-            angular_velocity += alpha*dt;
-            orientation = orientation + 0.5 * Math::Quaternion(0.0f, angular_velocity.x, angular_velocity.y, angular_velocity.z) * orientation * dt;
+            // Linear dynamics
+            Math::Vector3 a = force_accumulator * inv_mass;
+            linear_velocity += a * dt;
+            position += linear_velocity * dt;
+
+            // Transform inverse inertia tensor to world space: I_world^-1 = R * I_body^-1 * R^T
+            Math::Matrix3x3 R = Math::Matrix3x3::from_quaternion(orientation);
+            inv_inertia_tensor_world = R * inv_inertia_tensor_body * R.transposed();
+
+            // Angular dynamics
+            Math::Vector3 alpha = inv_inertia_tensor_world * torque_accumulator;
+            angular_velocity += alpha * dt;
+
+            // Quaternion orientation update
+            orientation = orientation + 0.5 * Math::Quaternion(0.0, angular_velocity.x, angular_velocity.y, angular_velocity.z) * orientation * dt;
             orientation.normalize();
 
             clear_forces();
